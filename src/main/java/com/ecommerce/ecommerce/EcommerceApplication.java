bcc1aa6a148b36b625e37637ecc68c19137ee6d9
Ansh Goyal <anshg1214@gmail.com>
2022-12-09 01:11:42 +0530

    feat: Add Docs for Endpoints

--- src/main/java/com/ecommerce/ecommerce/EcommerceApplication.java
+++ src/main/java/com/ecommerce/ecommerce/EcommerceApplication.java
@@ -1,13 +1,26 @@
 package com.ecommerce.ecommerce;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
 @SpringBootApplication
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
+	}
+
+    @Bean
+	public OpenAPI customOpenAPI(@Value("1.6.13") String appVersion) {
+		return new OpenAPI()
+				.components(new Components())
+				.info(new Info().title("Backend Documentation").version(appVersion));
 	}
 
 }
